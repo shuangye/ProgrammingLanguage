3c80910cb5c3eb8b20ca0b750dfb7154ee68831c
liumingyang <liumingyang@hzdusun.com>
2017-09-15 10:21:19 +0800

    Added a few macros.

--- osa/include/osa_macro.h
+++ osa/include/osa_macro.h
@@ -9,8 +9,12 @@
 
 
 #define OSA_clear(x)                                (memset(&(x), 0, sizeof(x)))
+#define OSA_arraySize(array)                        (sizeof(array) / sizeof((array)[0]))
 #define OSA_min(x, y)                               ((x) > (y) ? (y) : (x))
 #define OSA_max(x, y)                               ((x) > (y) ? (x) : (y))
+#define OSA_abs(x)                                  ((x) > 0 ? (x) : (-(x)))
+#define OSA_isInDeviation(x, value, dev)            ((x) >= ((value) - (dev)) && (x) <= ((value) + (dev)))
+#define OSA_isInRange(x, lower, upper)              ((x) >= (lower) && (x) <= (upper))
 #define OSA_roundUp(x, round)                       (((x) + (round) - 1) / (round) * (round))
 #define OSA_upperLimit(x, limit)                                                \
         do {                                                                    \
