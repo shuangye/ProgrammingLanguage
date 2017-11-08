fbc3e763f628eda17d463eba14409321c175f61a
shuangye <liumingyang@hzdusun.com>
2017-11-08 09:09:13 +0800

    Added Python interface for C functions.

--- python/binding_ctypes/dbm.h
+++ python/binding_ctypes/dbm.h
@@ -4,10 +4,12 @@
 
 #include <osa/osa.h>
 #include "dbm_definitions.h"
-// #include "dbm_utils.h"
+#include "dbm_utils.h"
 #include "dbm_entities.h"
-// #include "dbm_houses.h"
-// #include "dbm_persons.h"
+
+
+#define DBM_IS_ENTITY_TYPE_VALID(type)      (((type) >= DBM_ENTITY_TYPE_FIRST) && ((type) <= DBM_ENTITY_TYPE_LAST))
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,42 +34,157 @@ extern "C" {
         DBM_ENTITY_TYPE_FIRST                     = DBM_ENTITY_TYPE_HOUSE, 
         DBM_ENTITY_TYPE_LAST                      = DBM_ENTITY_TYPE_DEVICE_STATUS, 
     } DBM_EntityType;
+    
+
+    typedef enum DBM_DataSync {
+        DBM_DATA_SYNC_UNSYNCED = 0,
+        DBM_DATA_SYNC_SYNCED,
+    } DBM_DataSync;
       
 
     typedef int (*DBM_EntityFilter)(const DBM_EntityType entityType, const void *pEntity);
-    typedef struct DBM_GetOptions {
-        DBM_EntityType                 entityType;        
-        DBM_EntityFilter               filter;               /* returns 0: do not use this entity; returns non-0: use this entity. Can be used to implement complex filters */
-        Char                          *pConditions;          /* the conditions after SQL `WHERE` keyword */
+    typedef struct DBM_EntityOptions {
+        DBM_EntityType                 entityType;           /* [in] which type of entity do you want to get */
+        DBM_EntityFilter               filter;               /* [in] returns 0: do not use this entity; returns non-0: use this entity. Can be used to implement complex filters */
+        Char                          *pConditions;          /* [in] the conditions after SQL `WHERE` keyword */
+        Uint32                        *pCount;               /* [in][out] the caller fills the count of entities to get; the callee fills the actual count of entities got */
+        Uint32                         offset;               /* [in] entities count offset (0 based), relative to the first got entity */
+        void                          *pEntities;            /* [out] memory to store the returned entities; the memory shall be allocated by the caller */
+        
         /*  
-           Example:  Get at most 10 persons that are not synced:
-           int ret;
-           Uint32 maxCount = 10;           
-           DBM_GetOptions options;           
+           Example:  Get at most 50 persons that are not synced:
+           int ret;          
+           DBM_EntityOptions options;           
            OSA_clear(&options);
+           Uint32 count = 50;
            
            options.entityType = DBM_ENTITY_TYPE_PERSON;
            options.filter = NULL;
-           options.pConditions = "sync == 1"
-           void *pBuffer = malloc(sizeof(person) * maxCount);
-           ret = DBM_getEntity(handle, pBuffer, maxCount, &options);
+           options.pConditions = "sync = 1";
+           options.pCount = &count;
+           options.offset = 0;
+           options.pBuffer = malloc(sizeof(person) * (*options.pCount));
+           ret = DBM_getEntity(handle, &options);
          */
-    } DBM_GetOptions;
+    } DBM_EntityOptions;
         
 
-    
-    int DBM_init(const Char *pDbPath, DBM_Handle *pHandle);
+    /** @brief Initialize this module.    
+     *  @param pConnectionString [in]: Connection string is sth. like "User ID = papillon; Password = Hello; Server = localhost; Initial Catalog = SAC"
+     *  @param pHandle [out]: the handle to be used in other callings.
+     *  @return standard OSA status codes.
+     */
+    int DBM_init(const Char *pConnectionString, DBM_Handle *pHandle);
 
+
+    /** @brief De-initialize this module (release any allocated resources)
+    *  @param handle [in]: the handle inited by `DBM_init()`
+    *  @return standard OSA status codes.
+    */
     int DBM_deinit(DBM_Handle handle);
+        
 
-    /* not implemented yet */
-    // int DBM_getEntity(DBM_Handle handle, void *pBuffer, const Uint32 maxCount, const DBM_GetOptions *pOptions);
+    /** @brief Get the size (in bytes) of one entity type.
+    *  @param entityType [in]: the entity type you want to get its size.
+    *  @return entity size in bytes.
+    */
+    size_t DBM_getEntitySize(const DBM_EntityType entityType);
 
+
+    /** @brief Get entities count that matches the specified conditions.
+    *  @param handle [in]: the handle inited by `DBM_init()`
+    *  @param pOptions [in]: the caller shall fill these fields: entityType, pCount;
+    *                        these fields are conditional: filter, pConditions;
+    *                        these fields can be ignored: offset, pEntities.
+    *   NOTE:                the entities count is returned via the `pCount` field.
+    *  @return standard OSA status codes.
+    */
+    int DBM_getEntitiesCount(DBM_Handle handle, DBM_EntityOptions *pOptions);
+       
+
+    /** @brief Get entities that matches the specified conditions.
+    *  @param handle [in]: the handle inited by `DBM_init()`
+    *  @param pOptions [in]: the caller shall fill these fields: entityType, pCount, offset, pEntities;
+    *                        these fields are conditional: filter, pConditions;
+    *   NOTE:                expected entities count is specified via the `pCount` field when calling this function;
+    *                        when this function returns, actual `pCount` field is filled with actual count (<= specified count).
+    *  @return standard OSA status codes.
+    */
+    int DBM_getEntities(DBM_Handle handle, DBM_EntityOptions *pOptions);
+
+
+    /** @brief Get one entity that is not synced.
+    *  @param handle [in]: the handle inited by `DBM_init()`
+    *  @param entityType [in]: the entity type you want to get.
+    *  @param pEntity [out]: returned entity.
+    *  @param pAssociation: currently please specify NULL.
+    *  @return standard OSA status codes. If no unsynced entity of the specified type, it returns `OSA_STATUS_ENODATA`.
+    */
     int DBM_getUnsyncedEntity(DBM_Handle handle, const DBM_EntityType entityType, void *pEntity, void *pAssociation);
 
-    int DBM_insertEntity(DBM_Handle handle, const DBM_EntityType entityType, const void *pEntity, const void *pAssociation);
+
+    /** @brief Update specific fields of entities that match the specified conditions.
+    *  @param handle [in]: the handle inited by `DBM_init()`
+    *  @param pFieldValues[in]: fields and their values in standard SQL syntax, e.g., "field1 = value1, field2 = value2"
+    *  @param pOptions [in]: the caller shall fill these fields: entityType;
+    *                        these fields are conditional: pCount, filter, pConditions;
+    *                        these fields can be ignored: offset, pEntities.
+    *   NOTE:                if the `pCount` field is not NULL, it will be filled as the count of rows updated after this function returns.
+    *  @return standard OSA status codes.
+    */
+    int DBM_updateEntities(DBM_Handle handle, const Char *pFieldValues, DBM_EntityOptions *pOptions);
+
+
+    /** @brief Overwrite all fields of entities that match the specified conditions.
+    *          The caller is responsible to ensure that all fields are valid.
+    *  @param handle [in]: the handle inited by `DBM_init()`
+    *  @param pOptions [in]: the caller shall fill these fields: entityType, pCount, pEntities;
+    *                        these fields are conditional: filter, pConditions;
+    *                        these fields can be ignored: offset.
+    *   NOTE:                pCount shall alwasys be 1, and pEntities shall contain one entity.
+    *                        After this function returns, `pCount` will be filled as actual rows count that was/were overwritten.
+    *  @return standard OSA status codes.
+    */
+    int DBM_overwriteEntities(DBM_Handle handle, DBM_EntityOptions *pOptions);
+
+
+    /** @brief Insert one entity to database.
+    *          Note: all fields are assumed to be valid.
+    *  @param handle [in]: the handle inited by `DBM_init()`
+    *  @param entityType [in]: the entity type you want to insert.
+    *  @param pEntity [in]: the entity itself.
+    *                       After successful calling, it is updated to reflect the actual value inserted into database, 
+    *                       since some fields will be overwritten by this module.
+    *  @param pAssociation: currently please specify NULL.
+    *  @return standard OSA status codes.
+    */
+    int DBM_insertEntity(DBM_Handle handle, const DBM_EntityType entityType, void *pEntity, void *pAssociation);
+
+
+    /** @brief Insert one entity to database.
+    *          Note: silimar to `DBM_insertEntity()` but some fields may be incomplete.
+    *  @return standard OSA status codes.
+    */
+    int DBM_insertEntityFromVendor(DBM_Handle handle, const DBM_EntityType entityType, void *pEntity);
     
+
+    /** @brief Print all fields of en entity.
+    *  @param entityType [in]: the entity type you want to print.
+    *  @param pEntity [in]: the entity itself.
+    *  @return standard OSA status codes.
+    */
     int DBM_printEntity(const DBM_EntityType entityType, const void *pEntity);
+
+
+    /** @brief Delete entities that match the specified conditions.
+    *  @param handle [in]: the handle inited by `DBM_init()`    
+    *  @param pOptions [in]: the caller shall fill these fields: entityType;
+    *                        these fields are conditional: filter, pConditions, pCount;
+    *                        these fields can be ignored: offset, pEntities.
+    *   NOTE:                if the `pCount` field is not NULL, it will be filled as the count of rows deleted after this function returns.
+    *  @return standard OSA status codes.
+    */
+    int DBM_deleteEntity(DBM_Handle handle, DBM_EntityOptions *pOptions);
 
 #ifdef __cplusplus
 }
--- python/binding_ctypes/dbm.py
+++ python/binding_ctypes/dbm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
+import sys
 import ctypes
 import os
 import dbm_entities
@@ -46,24 +47,43 @@ class DBM_EntityType(CtypesEnum):
     # since this class inherits CtypesEnum, the `from_param` method can be omitted.
 
 
-gLibdbmPath = r"../build/libdbmanager.so"
+
+class DBM_EntityOptions(ctypes.Structure):
+    _fields_ = [
+        ("entityType"                , ctypes.c_int),
+        ("filter"                    , ctypes.c_void_p),
+        ("pConditions"               , ctypes.c_char_p),        
+        ("pCount"                    , ctypes.POINTER(ctypes.c_uint)),
+        ("offset"                    , ctypes.c_uint) ,
+        ("pEntities"                 , ctypes.c_void_p) 
+    ]
+
+
+
+
+gLibOsaPath = r'./libosa.so'
+gLibdbmPath = r"./libdbmanager.so"
+gLibSqlite3Path = r"./libsqlite3.so"
+gLibMysqlClientPath = r'libmysqlclient.so'
 gLibdbm = None
 gLibdbmHandle = ctypes.c_void_p()
 # global libdbm_path, libdbm, libdbm_handle
 
 
-def DBM_init(pDbPath):
+def DBM_init(connectionString):
     global gLibdbmPath, gLibdbm, gLibdbmHandle
 
-    if (None == pDbPath):
+    if (None == connectionString):
         return -1
 
+    ctypes.CDLL(gLibOsaPath, mode = ctypes.RTLD_GLOBAL)    
+    ctypes.CDLL(gLibMysqlClientPath, mode = ctypes.RTLD_GLOBAL)    
     gLibdbm = ctypes.cdll.LoadLibrary(gLibdbmPath)
     c_DBM_init = gLibdbm.DBM_init
     c_DBM_init.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_void_p)]
     c_DBM_init.restype = ctypes.c_int
     handle = ctypes.c_void_p()
-    ret = c_DBM_init(dbm_utilities.DBM_utlToString(pDbPath), handle) 
+    ret = c_DBM_init(dbm_utilities.DBM_utlToString(connectionString), handle) 
     # print("c_DBM_init returned {ret}, handle = {handle}".format(ret = ret, handle = handle))    
     return ret, handle
 
@@ -73,7 +93,21 @@ def DBM_deinit(handle):
     c_DBM_deinit.argtypes = [ctypes.c_void_p]
     c_DBM_deinit.restype = ctypes.c_int
     return c_DBM_deinit(handle)
-    
+
+
+def DBM_getEntitiesCount(handle, options):
+    c_DBM_getEntitiesCount = gLibdbm.DBM_getEntitiesCount;
+    c_DBM_getEntitiesCount.argtypes = [ctypes.c_void_p, POINTER(DBM_EntityOptions)];
+    c_DBM_getEntitiesCount.restype = ctypes.c_int;
+    return c_DBM_getEntitiesCount(handle, options);
+
+
+def DBM_getEntities(handle, options):
+    c_DBM_getEntities = gLibdbm.DBM_getEntities;
+    c_DBM_getEntities.argtypes = [ctypes.c_void_p, POINTER(DBM_EntityOptions)];
+    c_DBM_getEntities.restype = ctypes.c_int;
+    return c_DBM_getEntities(handle, options);
+
 
 def DBM_getUnsyncedEntity(handle, entityType):
     c_DBM_getUnsyncedEntity = gLibdbm.DBM_getUnsyncedEntity
@@ -91,6 +125,13 @@ def DBM_insertEntity(handle, entityType, entity):
     ret = c_DBM_insertEntity(handle, entityType, ctypes.addressof(entity), None)
     return ret
 
+def DBM_insertEntityFromVendor(handle, entityType, entity):
+    c_DBM_insertEntityFromVendor = gLibdbm.DBM_insertEntityFromVendor
+    c_DBM_insertEntityFromVendor.argtypes = [ctypes.c_void_p, DBM_EntityType, ctypes.c_void_p, ctypes.c_void_p]
+    c_DBM_insertEntityFromVendor.restype = ctypes.c_int
+    ret = c_DBM_insertEntityFromVendor(handle, entityType, ctypes.addressof(entity), None)
+    return ret
+
 
 def DBM_printEntity(entityType, entity):
     c_DBM_printEntity = gLibdbm.DBM_printEntity
@@ -99,37 +140,76 @@ def DBM_printEntity(entityType, entity):
     c_DBM_printEntity(entityType, ctypes.addressof(entity))
 
 
-def DBM_test():
+def DBM_test(connectionString):
+    def getCount():
+        count = ctypes.c_uint(0);
+        pCount = ctypes.pointer(count);
+        options = DBM_EntityOptions();
+        options.entityType = DBM_EntityType.DBM_ENTITY_TYPE_ACCESS_RECORD;
+        options.filter = None;
+        options.pConditions = b'sync = 1';
+        options.pCount = pCount;
+        options.offset = 0;
+        options.pEntities = None;
+        ret = DBM_getEntitiesCount(gLibdbmHandle, options);
+        print('There is/are {count} entity/entities of type {type} satisfied the query.'.format(count = pCount[0], type = options.entityType));
+
+    def getEntities():
+        count = ctypes.c_uint(1);
+        entity = dbm_entities.DBM_AccessRecord();
+        pCount = ctypes.pointer(count);
+        options = DBM_EntityOptions();
+        options.entityType = DBM_EntityType.DBM_ENTITY_TYPE_ACCESS_RECORD;
+        options.filter = None;
+        options.pConditions = b'sync = 1';
+        options.pCount = pCount;
+        options.offset = 0;
+        options.pEntities = ctypes.addressof(entity);
+        ret = DBM_getEntities(gLibdbmHandle, options);
+        if (0 == ret):
+            DBM_printEntity(options.entityType, entity);
+        else:
+            print("Failed to get entity: " + ret);
+
+    def insertEntity():
+        entity = dbm_entities.DBM_AccessRecord()
+        entity.cardno = dbm_utilities.DBM_utlToString("2715594")
+        # entity.person_uuid = dbm_utilities.DBM_utlToString('Person 人员 uuid 1234567890')
+        # entity.mac = dbm_utilities.DBM_utlToString("mac 1234")
+        entity.opentype = dbm_utilities.DBM_utlToString('C')
+        # entity.area_uuid = dbm_utilities.DBM_utlToString("area 区域 uuid 1234567890")
+        entity.slide_date = dbm_utilities.DBM_utlToString("20171019175200")
+        # entity.cdate = dbm_utilities.DBM_utlToString("20171019175200")
+        # entity.dev_uuid = dbm_utilities.DBM_utlToString("dev uuid 1234567890")
+        entity.dev_number = dbm_utilities.DBM_utlToString("设备编号 123456")
+        # entity.area_code = dbm_utilities.DBM_utlToString("area code")
+        entity.flag = 1
+        ret = DBM_insertEntityFromVendor(gLibdbmHandle, entityType, entity)
+        print("inserted entity")
+
+
     global gLibdbmPath, gLibdbm, gLibdbmHandle
-
     entityType = DBM_EntityType.DBM_ENTITY_TYPE_ACCESS_RECORD
-
-    ret, gLibdbmHandle = DBM_init("/home/papillon/dev/houses.db")
+    
+    ret, gLibdbmHandle = DBM_init(connectionString)
     # print("G_libdbm_handle = " + str(gLibdbmHandle))
 
-    entity = dbm_entities.DBM_AccessRecord()
-    entity.cardno = dbm_utilities.DBM_utlToString("Card no. 123")
-    entity.person_uuid = dbm_utilities.DBM_utlToString('Person 人员 uuid 1234567890')
-    entity.mac = dbm_utilities.DBM_utlToString("mac 1234")
-    entity.opentype = 1
-    entity.area_uuid = dbm_utilities.DBM_utlToString("area 区域 uuid 1234567890")
-    entity.slide_date = dbm_utilities.DBM_utlToString("20171019175200")
-    entity.cdate = dbm_utilities.DBM_utlToString("20171019175200")
-    entity.dev_uuid = dbm_utilities.DBM_utlToString("dev uuid 1234567890")
-    entity.area_code = dbm_utilities.DBM_utlToString("area code")
-    entity.flag = 1
-    ret = DBM_insertEntity(gLibdbmHandle, entityType, entity)
+    insertEntity();
+    getCount();
+    getEntities();
 
     ret, entity = DBM_getUnsyncedEntity(gLibdbmHandle, entityType)
     print("DBM_getUnsyncedEntity: ret = {ret}".format(ret = ret))
-
-    DBM_printEntity(entityType, entity)
-
+    
     ret = DBM_deinit(gLibdbmHandle)
     print("DBM_deinit: ret = {ret}".format(ret = ret))
 
 
 
 
-if "__main__" == __name__:
-    DBM_test()
+if "__main__" == __name__:    
+    if (len(sys.argv) < 2):
+        print("usage: {0} connection_string".format(sys.argv[0]))
+        sys.exit(-1)
+    else:
+        DBM_test(sys.argv[1])
